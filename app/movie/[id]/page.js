c8f51da8742c945ad6187dff998a6f9a1ca7cf86
codescaptain <ahmet-57-@hotmail.com>
2023-04-17 00:57:24 +0300

    Refactor to fetch movie data

--- app/movie/[id]/page.js
+++ app/movie/[id]/page.js
@@ -1,17 +1,7 @@
 import MovieContainer from '@/containers/movie'
 import { notFound } from "next/navigation";
 import React from 'react'
-import Movies from '@/mocks/movies.json'
-
-const API_URL = process.env.API_URL; 
-
-const getMovie = async (movieId) => {
-   const response = await fetch(`${API_URL}/movie/${movieId}?api_key=${process.env.THE_MOVIE_DB_API_KEY}&page=1`)
-
-   return response.json();
-}
-
-
+import {getMovie} from '@/services/movie'
 
 export default async function MoviePage({ params, searchParams }) {
     const movieDetail = await getMovie(params.id)

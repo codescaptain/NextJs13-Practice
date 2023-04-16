add0d466659ba370f5490f481d116486818e1d2e
codescaptain <ahmet-57-@hotmail.com>
2023-04-16 21:43:30 +0300

    Movie data fetching from API

--- app/[[...category]]/page.js
+++ app/[[...category]]/page.js
@@ -2,19 +2,68 @@ import HomeContainer from '@/containers/home'
 import React from 'react'
 import Movies from '@/mocks/movies.json'
 
+
+const API_URL = process.env.API_URL; 
+
+const getPopularMovies = async () => {
+   const response = await fetch(`${API_URL}/movie/popular?api_key=${process.env.THE_MOVIE_DB_API_KEY}&page=1`)
+
+   return response.json();
+}
+
+const getSingleCategory = async (genreId) => {
+  const response = await fetch(`${API_URL}/discover/movie?api_key=${process.env.THE_MOVIE_DB_API_KEY}&page=1&with_genres=${genreId}`)
+
+  return response.json();
+}
+
+const getTopRatedMovies = async () => {
+  const response = await fetch(`${API_URL}/movie/top_rated?api_key=${process.env.THE_MOVIE_DB_API_KEY}&page=1`)
+
+  return response.json();
+}
+
+const getCategories = async () => {
+  const response = await fetch(`${API_URL}/genre/movie/list?api_key=${process.env.THE_MOVIE_DB_API_KEY}&page=1`)
+
+  return response.json();
+}
+
 function delay(ms){
     return new Promise((resolve) => setTimeout(resolve,ms))
 }
 
 export default async function Home({params}) {
-    await delay(20000)
+
+  const popularPromise = getPopularMovies();
+  const topRatedPromise = getTopRatedMovies();
+  const categoryPromise = getCategories();
+
+  const [
+    {results: popularMovies},
+    {results: topRatedMovies},
+    {genres: categories},
+  ] = await Promise.all([popularPromise,
+                        topRatedPromise,
+                        categoryPromise
+                      ]) 
+
   let selectedCategory;
-  if (params.category?.length > 0){selectedCategory = true }
+  if (params.category?.length > 0){
+    const { results } = await getSingleCategory(params.category[0])
+    selectedCategory = results;
+   }
+
 
   return (
-    <HomeContainer selectedCategory= {{
-        id: params.category?.[0] ?? "",
-        movies: selectedCategory ? Movies.results.slice(1,7) : []
-    }}/>
+    <HomeContainer 
+    popularMovies = {popularMovies}
+    topRatedMovies = {topRatedMovies}
+    categories = {categories}
+      selectedCategory= {{
+          id: params.category?.[0] ?? "",
+          movies: selectedCategory ? selectedCategory.slice(1,7) : []
+      }}
+    />
   )
 }
--- app/movie/[id]/page.js
+++ app/movie/[id]/page.js
@@ -3,8 +3,18 @@ import { notFound } from "next/navigation";
 import React from 'react'
 import Movies from '@/mocks/movies.json'
 
-export default function MoviePage({ params, searchParams }) {
-    const movieDetail = Movies.results.find((movie) => movie.id.toString() === params.id)
+const API_URL = process.env.API_URL; 
+
+const getMovie = async (movieId) => {
+   const response = await fetch(`${API_URL}/movie/${movieId}?api_key=${process.env.THE_MOVIE_DB_API_KEY}&page=1`)
+
+   return response.json();
+}
+
+
+
+export default async function MoviePage({ params, searchParams }) {
+    const movieDetail = await getMovie(params.id)
     
     if(!movieDetail){
       notFound()
